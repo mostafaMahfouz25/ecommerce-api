4b70367c605bbbacc110a3143968266ea1b397a2
mostafa <eraasoft2019@gmail.com>
2019-08-18 23:26:58 +0200

    product resource

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Models;
+use App\Models\Review;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Models;
+use App\Models\Product;
+
 
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
